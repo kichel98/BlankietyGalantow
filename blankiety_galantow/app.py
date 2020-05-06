0a81394798803a267f5cde07bf4648ad2f9b324d
ketrab321 <37443550+ketrab321@users.noreply.github.com>
2020-05-06 17:29:37 +0200

    Server prototype

--- blankiety_galantow/app.py
+++ blankiety_galantow/app.py
@@ -6,45 +6,11 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
 app.mount("/vendor", StaticFiles(directory="../docs/prototype/vendor"), name="vendor")
 app.mount("/js", StaticFiles(directory="../docs/prototype/servers-vue/js"), name="js")
 app.mount("/css", StaticFiles(directory="../docs/prototype/servers-vue/css"), name="css")
-
+app.mount("/game/js", StaticFiles(directory="../docs/prototype/game-vue/js"), name="game/js")
+app.mount("/game/css", StaticFiles(directory="../docs/prototype/game-vue/css"), name="game/css")
 
 
 @app.get("/")
@@ -52,10 +18,15 @@ async def get():
     f = codecs.open('../docs/prototype/servers-vue/index.html','r', 'utf-8')
     return HTMLResponse(f.read())
 
+@app.get("/game/{game_id}")
+async def connect_to_game(game_id):
+    f = codecs.open('../docs/prototype/game-vue/index.html','r', 'utf-8')
+    return HTMLResponse(f.read())
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+
+@app.websocket("/connect/{game_id}")
+async def websocket_endpoint(websocket: WebSocket, game_id: int):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        await websocket.send_text(f"[Game {game_id}] Message text was: {data}")
