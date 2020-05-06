45313a2ea4b17ae777a25dd300d5c31c2ea78d5e
ketrab321 <37443550+ketrab321@users.noreply.github.com>
2020-05-06 18:08:51 +0200

    Websocket chat test

--- blankiety_galantow/app.py
+++ blankiety_galantow/app.py
@@ -1,10 +1,14 @@
 import uvicorn
 import codecs
+from typing import List, Dict
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+def run():
+    uvicorn.run(app, host="localhost", port=8000)
 
 app.mount("/vendor", StaticFiles(directory="./docs/prototype/vendor"), name="vendor")
 app.mount("/js", StaticFiles(directory="./docs/prototype/servers-vue/js"), name="js")
@@ -12,8 +16,12 @@ app.mount("/css", StaticFiles(directory="./docs/prototype/servers-vue/css"), nam
 app.mount("/game/js", StaticFiles(directory="./docs/prototype/game-vue/js"), name="game/js")
 app.mount("/game/css", StaticFiles(directory="./docs/prototype/game-vue/css"), name="game/css")
 
-def run():
-    uvicorn.run(app, host="localhost", port=8000)
+class Server:
+    def __init__(self):
+        self.tables = {}
+    tables: Dict[int, List[WebSocket]]
+
+server = Server()
 
 @app.get("/")
 async def get():
@@ -29,6 +37,12 @@ async def connect_to_game(game_id):
 @app.websocket("/connect/{game_id}")
 async def websocket_endpoint(websocket: WebSocket, game_id: int):
     await websocket.accept()
+    if game_id in server.tables:
+        server.tables[game_id].append(websocket)
+    else:
+        server.tables[game_id] = [websocket]
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"[Game {game_id}] Message text was: {data}")
+        for socket in server.tables[game_id]:
+            await socket.send_text(f"[Game {game_id}] Message text was: {data}")
+        
