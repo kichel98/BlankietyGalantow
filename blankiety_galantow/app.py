0b232f430b87f7e6955d28ab48b7b80ea38d024a
kichel98 <44356453+kichel98@users.noreply.github.com>
2020-05-07 22:28:53 +0200

    :tada: Send static files from HTTP server

--- blankiety_galantow/app.py
+++ blankiety_galantow/app.py
@@ -1,12 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from blankiety_galantow.routers import router
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.mount("/css", StaticFiles(directory="resources/css"), name="css")
+app.mount("/js", StaticFiles(directory="resources/js"), name="js")
+app.mount("/vendor", StaticFiles(directory="resources/vendor"), name="vendor")
+app.mount("/game", StaticFiles(directory="resources/game"), name="game")
+app.include_router(router)
 
 
 def run():
