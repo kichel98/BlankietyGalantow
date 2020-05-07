5f52130b7b7f34ecce52be994844a8b4be1a8a44
Marta Sokołowska <msokolowska@localhost.localdomain>
2020-05-07 20:40:41 +0200

    Created server list API draft mentioned in issue #6

--- blankiety_galantow/app.py
+++ blankiety_galantow/app.py
@@ -1,12 +1,31 @@
 import uvicorn
 from fastapi import FastAPI
+from random import randint
+
+
+class Server:
+    def __init__(self, name: str, server_id: int, server_address: str, number_of_players: int, number_of_seats: int):
+        self.name = name
+        self.server_id = server_id
+        self.server_address = server_address
+        self.number_of_players = number_of_players
+        self.number_of_seats = number_of_seats
+
+    name: str
+    server_id: int
+    server_address: str
+    number_of_players: int
+    number_of_seats: int
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/servers")
+async def servers():
+    servers_dict = {i: vars(Server("server" + str(i), i, "addr" + str(i), randint(0, i), randint(0, i))) for i in range(10)}
+    return {"servers": servers_dict}
+
 
 
 def run():
