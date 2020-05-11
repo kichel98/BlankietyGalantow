2b2bcb52212d7da0fb60f24fae9ecfa31db0442d
ketrab321 <37443550+ketrab321@users.noreply.github.com>
2020-05-11 18:07:42 +0200

    Routers change

--- blankiety_galantow/app.py
+++ blankiety_galantow/app.py
@@ -8,7 +8,6 @@ from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from blankiety_galantow.routers import router
-from blankiety_galantow.connect_router import connect_router
 # Importing classes
 from .classes.Server import Server
 
@@ -21,9 +20,8 @@ app.mount("/js", StaticFiles(directory="resources/js"), name="js")
 app.mount("/vendor", StaticFiles(directory="resources/vendor"), name="vendor")
 app.mount("/game/css", StaticFiles(directory="resources/game/css"), name="game/css")
 app.mount("/game/js", StaticFiles(directory="resources/game/js"), name="game/js")
-# Attaching routers to app
+# Attaching router to app
 app.include_router(router)
-app.include_router(connect_router)
 
 # Starting server using uvicorn
 def run():
--- blankiety_galantow/routers.py
+++ blankiety_galantow/routers.py
@@ -1,10 +1,38 @@
 import codecs
-from fastapi import APIRouter
+import json
+from fastapi import APIRouter, FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
+import blankiety_galantow.app as app
+from fastapi.staticfiles import StaticFiles
+# Importing classes
+from .classes.Player import Player
+from .classes.Server import Server
 
 router = APIRouter()
-
 
 @router.get("/", response_class=HTMLResponse)
 async def root():
     return codecs.open("resources/index.html", "r", "utf-8").read()
+
+@router.get("/game/{game_id}")
+async def connect_to_game(game_id):
+    f = codecs.open('resources/game/index.html','r', 'utf-8')
+    return HTMLResponse(f.read())
+
+
+@router.websocket("/connect/{game_id}")
+async def websocket_endpoint(websocket: WebSocket, game_id: int, username: str):
+    await websocket.accept()
+    if game_id in app.server.tables:
+        app.server.tables[game_id].append(Player(username, websocket))
+    else:
+        app.server.tables[game_id] = [Player(username, websocket)]
+    while True:
+        data = await websocket.receive_text()
+        
+        for user in app.server.tables[game_id]:
+            message = {
+                "user": username,
+                "message": data
+            }
+            await user.socket.send_text(json.dumps(message))
