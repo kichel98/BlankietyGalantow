6269c4553daffe437e4bc45b52615e44757cb19b
Marta Sokołowska <msokolowska@localhost.localdomain>
2020-05-12 15:42:19 +0200

    modified room structure and renamed server to room #6

--- blankiety_galantow/app.py
+++ blankiety_galantow/app.py
@@ -16,4 +16,4 @@ app.include_router(router)
 
 
 def run():
-    uvicorn.run(app, host="localhost", port=80)
+    uvicorn.run(app, host="localhost", port=8080)
--- blankiety_galantow/routers.py
+++ blankiety_galantow/routers.py
@@ -2,7 +2,7 @@ import codecs
 from random import randint
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
-from blankiety_galantow.models.server import Server
+from blankiety_galantow.models.room import Room
 
 
 router = APIRouter()
@@ -12,7 +12,7 @@ router = APIRouter()
 async def root():
     return codecs.open("resources/index.html", "r", "utf-8").read()
 
-@router.get("/servers")
-async def servers():
-    servers_dict = {i: vars(Server("server" + str(i), i, "addr" + str(i), randint(0, i), randint(0, i))) for i in range(10)}
-    return {"servers": servers_dict}
+@router.get("/api/rooms")
+async def rooms():
+    rooms_dict = {i: take(4, iteritems(vars(Room("server" + str(i), i, "addr" + str(i), randint(0, i), randint(0, i), [], {}, {})))) for i in range(10)}
+    return {"rooms": rooms_dict}
