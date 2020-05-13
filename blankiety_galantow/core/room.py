c71b7b6d10ec6f782361f6c24b6c4c663bba42dc
Sebastian <kyriet.sebastian@gmail.com>
2020-05-13 13:19:18 +0200

    Local Storage support

--- blankiety_galantow/core/room.py
+++ blankiety_galantow/core/room.py
@@ -19,7 +19,7 @@ class Room:
     async def add_player_and_listen(self, player: Player):
         """Add new player to the room and start listening."""
         self.players.append(player)
-        await self.send_chat_log(f"Gracz '{player.name}' dołączył do pokoju.")
+        await self.send_chat_message_from_system(f"Gracz '{player.name}' dołączył do pokoju.")
         await self.listen_to_player(player)
 
     async def listen_to_player(self, player):
@@ -30,7 +30,7 @@ class Room:
                 await self.process_message(player, msg)
         except WebSocketDisconnect:
             self.players.remove(player)
-            await self.send_chat_log(f"Gracz '{player.name}' opuścił pokój.")
+            await self.send_chat_message_from_system(f"Gracz '{player.name}' opuścił pokój.")
 
     async def process_message(self, player: Player, data: Dict):
         """Process raw JSON message (data) from player."""
@@ -40,26 +40,26 @@ class Room:
         if data["type"] == "ERROR" and "message" in data:
             print(f"ERROR: {data['message']}")
         if data["type"] == "CHAT_MESSAGE" and "message" in data:
-            await self.send_chat_message(player.name, data["message"])
-
+            await self.send_chat_message_from_user(player.name, data["message"])
         # TODO: other types of messages
 
-    async def send_chat_message(self, sender_name: str, msg: str):
-        """Send message to all players in this room."""
-        for player in self.players:
-            data = {
-                "log": False,
-                "user": sender_name,
-                "message": msg
-            }
-            await player.send_json(data)
+    async def send_chat_message_from_user(self, sender_name: str, msg: str):
+        """Send message from player to all players in this room."""
+        await self.send_chat_message(sender=sender_name, message=msg)
 
-    async def send_chat_log(self, msg: str):
-        """Send chat log from the game to all players."""
+    async def send_chat_message_from_system(self, msg: str):
+        """Send chat message from the system to all players."""
+        await self.send_chat_message(sender="Gra", message=msg, as_system=True)
+
+    async def send_chat_message(self, sender, message, as_system=False):
+        """Send chat message to all players."""
         for player in self.players:
             data = {
-                "log": True,
-                "user": "Gra",
-                "message": msg
+                "type": "CHAT_MESSAGE",
+                "message": {
+                    "log": as_system,
+                    "user": sender,
+                    "text": message
+                }
             }
             await player.send_json(data)
