f48271a2fd605f757685eb46b9670e5115b4240b
kichel98 <44356453+kichel98@users.noreply.github.com>
2020-05-13 19:01:22 +0200

    Feat/fetch rooms (#25)
    * Fetch rooms from api and rename server to room
    
    * Fix class name after refactor
    
    * Change reference name in main.js
    
    Co-authored-by: Kyriet <kyriet.sebastian@gmail.com>

--- blankiety_galantow/resources/js/main.js
+++ blankiety_galantow/resources/js/main.js
@@ -1,40 +1,28 @@
 const app = new Vue({
     el: '#container',
+    data: {
+        username: window.localStorage.getItem("username") || "Bober",
+        rooms: []
+    },
+    // instead of `created` lifecycle hook, we can use `mounted`, if needed
+    created: function() {
+      this.fetchRooms()
+    },
     methods: {
+        fetchRooms: function() {
+            fetch("/api/rooms")
+                .then(response => response.json())
+                .then(data => {
+                    this.rooms = data.rooms;
+                })
+        },
         connect: function(id) {
             window.location.pathname = "game/" + id;
         }
     },
     watch: {
         username: function(newName) {
-            window.localStorage.setItem("username", this.username);
+            window.localStorage.setItem("username", newName);
         }
-    },
-    /* Some mockup data */
-    data: {
-        username: window.localStorage.getItem("username") || "Bober",
-        servers: [{
-                id: 1,
-                name: "Alpha",
-                players: 1,
-                maxPlayers: 6,
-                open: true
-            },
-            {
-                id: 2,
-                name: "Beta",
-                players: 3,
-                maxPlayers: 6,
-                open: false
-            },
-            {
-                id: 3,
-                name: "Charlie",
-                players: 6,
-                maxPlayers: 6,
-                open: true
-            }
-        ]
     }
-
 });
