8c81f026e321e3ab56848ba81e2d6ae61ae66b4a
ketrab321 <37443550+ketrab321@users.noreply.github.com>
2020-05-18 13:31:52 +0200

    Storing players hands in dictionary

--- blankiety_galantow/core/game_master.py
+++ blankiety_galantow/core/game_master.py
@@ -6,6 +6,8 @@ from .player import Player
 from typing import Dict, List
 
 class GameMaster:
+    white_deck: Deck
+    black_deck: Deck
     players_hands: Dict[Player, List[WhiteCard]]
     cards_selected: Dict[Player, List[WhiteCard]]
     black_card: BlackCard
@@ -14,17 +16,19 @@ class GameMaster:
         self.white_deck = white_deck
         self.black_deck = black_deck
         self.players_hands = {}
+        self.cards_selected = {}
         self.black_card = black_deck.get_card()
 
     async def new_player(self, player: Player):
-        self.players_hands[Player] = []
+        self.players_hands[player] = self.white_deck.get_cards(10)
+        self.cards_selected[player] = []
         await self.fill_players_hand(player)
         await self.send_black_card(player)
 
     async def fill_players_hand(self, player: Player):
         message = {
             "type": "PLAYER_HAND",
-            "cards": [card.__dict__ for card in self.white_deck.get_cards(10)]
+            "cards": [card.__dict__ for card in self.players_hands[player]]
         }
         await player.send_json(message)
 
