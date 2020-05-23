0b295f899a6f28015f3ce3a242d61b5a4876a273
ketrab321 <37443550+ketrab321@users.noreply.github.com>
2020-05-23 17:34:01 +0100

    Added player state to Player

--- blankiety_galantow/core/player.py
+++ blankiety_galantow/core/player.py
@@ -2,13 +2,22 @@ from json import JSONDecodeError
 from fastapi import WebSocket
 from fastapi.websockets import WebSocketDisconnect
 from .kick_exception import KickException
+from enum import Enum
 
 class Player:
     def __init__(self, socket: WebSocket, username: str):
         self.name = username
         self.socket = socket
         self.id = None
+        self.player_state = PlayerState.choosing
 
+    def set_player_state(self, state: PlayerState):
+        self.player_state = state
+
+    @property
+    def state(self):
+        return self.state.name
+        
     async def kick(self, reason: str):
         kick_reason = {
             "type": "KICK",
@@ -32,3 +41,8 @@ class Player:
     async def send_json(self, data):
         """Send JSON data to the player via socket."""
         await self.socket.send_json(data)
+
+class PlayerState(Enum):
+    master = 1
+    choosing = 2
+    ready = 3
