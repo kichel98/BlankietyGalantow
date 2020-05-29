581f2ab0205b8c3845c51b1977010eae74e2abb6
Unknown <piotr.andrzejewski00@gmail.com>
2020-05-29 19:44:33 +0200

    #51 Interval updates of rooms list

--- blankiety_galantow/resources/js/components.js
+++ blankiety_galantow/resources/js/components.js
@@ -1,19 +1,30 @@
+export const mixin = {
+    methods: {
+        canRoomAcceptNewPlayer: function (room) {
+            return room.open && room.players < room.maxPlayers
+        }
+    }
+};
+
 Vue.component('room-item', {
+    mixins: [mixin],
     props: {
         room: Object
     },
     template: `
         <li>
-            <div class="room-item hvr-icon-grow hvr-radial-out">
+            <div :class="canRoomAcceptNewPlayer(room) 
+                    ? 'room-item hvr-icon-grow hvr-radial-out pointer' 
+                    : 'room-item not-allowed'">
                 <i class="icon-group room-icon w3-hide-small"></i>
                 <div class="room-info">
                     <span class="w3-large">{{room.name}}</span><br>
                     <span>Gracze: {{room.players}}/{{room.maxPlayers}}</span>
                 </div>
                 <div class="room-status">
-                    <i v-if="room.open && room.players < room.maxPlayers" class="hvr-icon icon-lock-open open"></i>
+                    <i v-if="canRoomAcceptNewPlayer(room)" class="hvr-icon icon-lock-open open"></i>
                     <i v-else-if="!room.open" class="hvr-icon icon-lock-closed closed"></i>
-                    <i v-else="!room.open" class="hvr-icon icon-lock-closed-alt full"></i>
+                    <i v-else class="hvr-icon icon-lock-closed-alt full"></i>
                 </div>
             </div>
         </li>
@@ -26,7 +37,7 @@ Vue.component('room-create-button', {
     },
     template: `
         <li>
-            <div class="room-item w3-teal w3-bottombar w3-border-black hvr-icon-grow hvr-radial-out">
+            <div class="room-item w3-teal w3-bottombar w3-border-black hvr-icon-grow hvr-radial-out pointer">
                 <i class="icon-group room-icon w3-hide-small"></i>
                 <div class="room-info">
                     <span class="w3-large">Nowy pokój</span><br>
