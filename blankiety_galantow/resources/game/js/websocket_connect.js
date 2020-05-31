d3a62b1cac658d6081ebd3165b7160ebf859b990
Sebastian <kyriet.sebastian@gmail.com>
2020-05-31 21:54:38 +0200

    Dynamic Websocket URL generation

--- blankiety_galantow/resources/game/js/websocket_connect.js
+++ blankiety_galantow/resources/game/js/websocket_connect.js
@@ -4,6 +4,8 @@ function connect() {
     const game_id = parts[parts.length - 1];
     const username = window.localStorage.getItem("username") || "Bezimienny";
     const port = window.location.port ? ":" + window.location.port : "";
-    const webSocketUrl = "ws://localhost" + port + "/connect/" + game_id + "?username=" + username;
+    const host = window.location.hostname;
+    const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
+    const webSocketUrl = protocol + "//" + host + port + "/connect/" + game_id + "?username=" + username;
     return new WebSocket(webSocketUrl);
 }
