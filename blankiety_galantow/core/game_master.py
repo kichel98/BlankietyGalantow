dcc13822b03ae39ce74623cd020d922cf246bf0f
ketrab321 <37443550+ketrab321@users.noreply.github.com>
2020-06-06 16:47:11 +0100

    Catching all exceptions

--- blankiety_galantow/core/game_master.py
+++ blankiety_galantow/core/game_master.py
@@ -99,7 +99,7 @@ class GameMaster:
                 await self.timer.task
             except asyncio.CancelledError:
                 pass
-            except KickException:
+            except Exception:
                 raise
         self.timer = Timer(self.selecting_time, self.handle_timeout)
         
